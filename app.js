dfd420272f32be2ebf2b35f0f508917d3ee5c8af
Yeongil Yu <polosesy@nate.com>
2020-06-09 14:16:50 +0900

    2.2 Recap!

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 
+canvas.width = 700;
+canvas.height = 700;
+
 ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
 
@@ -18,9 +21,11 @@ function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
   if (!painting) {
+    console.log("creating path in", x, y);
     ctx.beginPath();
     ctx.moveTo(x, y);
   } else {
+    console.log("creating line in", x, y);
     ctx.lineTo(x, y);
     ctx.stroke();
   }
